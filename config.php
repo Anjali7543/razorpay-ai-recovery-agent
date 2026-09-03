85f2851699ad9fe0b3ff2f7e5cb387a06d9f7af3
Anjali7543 <kkumarianjalichaudhary@gmail.com>
2026-09-04 00:23:37 +0530

    Add configuration file for database connection

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$key_id = "rzp_test_YOURKEY";
+$key_secret = "YOURSECRET";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "razorpay_recovery";
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+?>
